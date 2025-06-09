8e54201d6c0f4e0425ad9700e994c105555abfe1
LuizGermano <mgermano578@gmail.com>
2025-06-09 17:54:50 -0300

    Update enviar_email.php

--- enviar_email.php
+++ enviar_email.php
@@ -11,13 +11,13 @@ function enviarConfirmacaoEmail($para, $assunto, $mensagem) {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'seuemail@gmail.com';         // <-- Substitua aqui
-        $mail->Password = 'ertfhrjsijxdlwdfz';            // <-- Substitua aqui (senha de app do Gmail)
+        $mail->Username = 'mgermano578@gmail.com';         // <-- Substitua aqui
+        $mail->Password = 'bjmhrjsijxdlwdfz';            // <-- Substitua aqui (senha de app do Gmail)
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
         // Remetente e destinatário
-        $mail->setFrom('seuemail@gmail.com', 'Sistema de Votação');
+        $mail->setFrom('mgermano578@gmail.com', 'Sistema de Votação');
         $mail->addAddress($para);
 
         // Conteúdo do e-mail
